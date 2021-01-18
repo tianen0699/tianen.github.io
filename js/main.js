2f4483d70f7c9a67d89916ad33cb5c768ed19a9f
Jeryl <jerylteo@outlook.com>
2021-01-18 12:25:32 +0800

    Portfolio Section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
+    initMenu();
+
+    initSwipers();
+});
+
+function initSwipers() {
+    const swiper = new Swiper('.swiper-illustrator', {
+        scrollbar: '.swiper-scrollbar',
+        slidesPerView: 1,
+        spaceBetween: 10,
+        breakpoints: {
+            768: {
+                slidesPerView: 2, 
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+            1280: {
+                slidesPerView: 4,
+                spaceBetween: 30
+            }
+        }
+    });
+    console.log(swiper);
+}
+
+function initMenu() {
     let menuBtn = document.getElementById('menuBtn');
     let menu = document.getElementById('menu');
 
     menuBtn.addEventListener('click', ()=> {
         menu.classList.toggle("hidden");
     })
-});
+}
